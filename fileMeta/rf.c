ed5131188d0e2f76b25323624a05058b32f56759
Morris He <hexinmin@gmail.com>
2023-12-03 19:03:09 +0800

    adjust format rf.c

---
+++ fileMeta/rf.c
@@ -0,0 +1,76 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "ds_err.h"
+
+void show_status(int num)
+{
+	if (num <= 0) {
+		return;
+	}
+	switch (num % 4) {
+		case 0:
+			printf(" -\r");
+			fflush(NULL);
+			break;
+		case 1:
+			printf(" \\\r");
+			fflush(NULL);
+			break;
+		case 2:
+			printf(" |\r");
+			fflush(NULL);
+			break;
+		case 3:
+			printf(" /\r");
+			fflush(NULL);
+			break;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned long count, i, j, k;
+
+	if (argc != 2) {
+		err_quit("USAGE: %s count", argv[0]);
+	}
+	count = strtoul(argv[1], NULL, 10);
+
+	for (i = 0; i < 1024; i++) {
+		char dir_l1[4096] = { 0 };
+
+		snprintf(dir_l1, sizeof(dir_l1), "dir_level1_%ld", i);
+		if (chdir(dir_l1) < 0) {
+			err_sys("chdir(%s) error", dir_l1);
+		}
+		for (j = 0; j < 4096; j++) {
+			char dir_l2[4096] = { 0 };
+
+			snprintf(dir_l2, sizeof(dir_l2), "dir_level2_%ld", j);
+			if (chdir(dir_l2) < 0) {
+				err_sys("chdir(%s) error", dir_l2);
+			}
+			for (k = 0; k < count; k = k + 2) {
+				char file[4096] = { 0 };
+				snprintf(file, sizeof(file), "file_%ld", k);
+				if (unlink(file) < 0) {
+					err_sys("unlink(%s) error", file);
+				}
+			}
+			if (chdir("..") < 0) {
+				err_sys("chdir(..) error");
+			}
+		}
+		show_status(j + 1);
+		if (chdir("..") < 0) {
+			err_sys("chdir(..) error");
+		}
+	}
+
+	return 0;
+}
